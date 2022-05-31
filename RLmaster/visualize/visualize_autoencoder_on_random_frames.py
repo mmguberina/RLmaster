2894535ebc4eb26ca1738fd637e99a4f7939944f
m-guberina <gubi.guberina@gmail.com>
2022-05-31 20:15:06 +0200

    dqn on rae

--- RLmaster/visualize/visualize_autoencoder_on_random_frames.py
+++ RLmaster/visualize/visualize_autoencoder_on_random_frames.py
@@ -5,7 +5,7 @@ from PIL import Image
 import imagehash
 from RLmaster.util.atari_wrapper import make_atari_env, AutoencoderEnv
 #from RLmaster.latent_representations.autoencoder_nn import CNNEncoder, CNNDecoder
-from RLmaster.latent_representations.autoencoder_nn import CNNEncoderNew, CNNDecoderNew
+from RLmaster.latent_representations.autoencoder_nn import CNNEncoderNew, CNNDecoderNew, RAE_ENC, RAE_DEC
 from RLmaster.util.save_load_hyperparameters import load_hyperparameters
 from collections import deque
 
@@ -20,13 +20,15 @@ import cv2
 # in the encoder class and stop this nonsense
 
 #features_dim = 576
-features_dim = 3136
+#features_dim = 3136
+features_dim = 50
 
 #log_path = "../../experiments/latent_only/log/PongNoFrameskip-v4/unlabelled_experiment/"
 #log_path = "../../experiments/latent_only/log/PongNoFrameskip-v4/training_preloaded_buffer_fs_1/"
 #log_path = "../../experiments/latent_only/log/PongNoFrameskip-v4/ae_trained_as_policy/"
 #log_path = "../../log/dqn_ae_parallel_good_arch_fs_4_passing_q_grads/"
-log_path = "../../log/ae_single-frame-trained_as_policy_3136_3/"
+#log_path = "../../log/ae_single-frame-trained_as_policy_3136_4/"
+log_path = "../../log/rae_single-frame-trained_as_policy_1/"
 #log_path = "../../experiments/latent_only/log/PongNoFrameskip-v4/ae_single-frame-trained_as_policy_3136/"
 #log_path_enc_dc = "../../experiments/latent_only/log/PongNoFrameskip-v4/"
 args = load_hyperparameters(log_path)
@@ -50,14 +52,16 @@ if args.latent_space_type == "forward-frame-predictor":
 #encoder.load_state_dict(torch.load("../../experiments/latent_only/encoder_features_dim_{}.pt".format(features_dim), map_location=torch.device('cpu')))
 #decoder.load_state_dict(torch.load("../../experiments/latent_only/decoder_features_dim_{}.pt".format(features_dim), map_location=torch.device('cpu')))
 
-encoder_name = "checkpoint_encoder_epoch_2.pth"
-decoder_name = "checkpoint_decoder_epoch_2.pth"
+encoder_name = "checkpoint_encoder_epoch_18.pth"
+decoder_name = "checkpoint_decoder_epoch_12.pth"
 #encoder_name = "encoder.pth"
 #decoder_name = "decoder.pth"
 #encoder_name = "encoder_features_dim_3136.pt"
 #decoder_name = "decoder_features_dim_3136.pt"
-encoder = CNNEncoderNew(observation_shape=observation_shape, features_dim=features_dim, device='cpu')
-decoder = CNNDecoderNew(observation_shape=observation_shape, n_flatten=encoder.n_flatten, features_dim=features_dim)
+#encoder = CNNEncoderNew(observation_shape=observation_shape, features_dim=features_dim, device='cpu')
+encoder = RAE_ENC("cpu", observation_shape, features_dim)
+#decoder = CNNDecoderNew(observation_shape=observation_shape, n_flatten=encoder.n_flatten, features_dim=features_dim)
+decoder = RAE_DEC("cpu", observation_shape, features_dim)
 #print(torch.load(log_path + encoder_name, map_location=torch.device('cpu')))
 encoder.load_state_dict(torch.load(log_path + encoder_name, map_location=torch.device('cpu'))['encoder'])
 decoder.load_state_dict(torch.load(log_path + decoder_name, map_location=torch.device('cpu'))['decoder'])
--- experiments/dqn_on_latent/atari_dqn_on_rae.py
+++ experiments/dqn_on_latent/atari_dqn_on_rae.py
@@ -11,9 +11,7 @@ from RLmaster.util.save_load_hyperparameters import save_hyperparameters
 
 from tianshou.data import Collector, VectorReplayBuffer
 from tianshou.env import ShmemVectorEnv
-# TODO write the autoencoder only policy
-#from RLmaster.policy.autoencoder_only import AutoencoderOnly
-from RLmaster.policy.random import RandomPolicy
+from RLmaster.policy.dqn_fixed import DQNPolicy
 from RLmaster.latent_representations.autoencoder_learning_as_policy_wrapper import AutoencoderLatentSpacePolicy
 from RLmaster.latent_representations.autoencoder_nn import RAE_ENC, RAE_DEC, CNNEncoderNew, CNNDecoderNew
 from RLmaster.util.collector_on_latent import CollectorOnLatent
@@ -29,48 +27,53 @@ action are all random sampled
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--task', type=str, default='PongNoFrameskip-v4')
-#    parser.add_argument('--latent-space-type', type=str, default='forward-frame-predictor')
     parser.add_argument('--latent-space-type', type=str, default='single-frame-predictor')
-#    parser.add_argument('--latent-space-type', type=str, default='inverse-dynamics-model')
-    parser.add_argument('--pass-q-grads-to-encoder', type=bool, default=False)
+    parser.add_argument('--pass-q-grads-to-encoder', type=bool, default=True)
     parser.add_argument('--alternating-training-frequency', type=int, default=1000)
-    parser.add_argument('--features_dim', type=int, default=50)
+    parser.add_argument('--features-dim', type=int, default=50)
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument("--scale-obs", type=int, default=0)
+    parser.add_argument('--eps-test', type=float, default=0.005)
+    parser.add_argument('--eps-train', type=float, default=1.)
+    parser.add_argument('--eps-train-final', type=float, default=0.05)
     parser.add_argument('--buffer-size', type=int, default=100000)
 #    parser.add_argument('--buffer-size', type=int, default=100)
-    parser.add_argument('--lr', type=float, default=0.001)
-    # TODO understand where exactly this is used and why
-    # it's probably how often you update the target policy network in deep-Q
-#    parser.add_argument('--target-update-freq', type=int, default=500)
-    parser.add_argument('--target-update-freq', type=int, default=5)
-#    parser.add_argument('--epoch', type=int, default=100)
+    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--gamma', type=float, default=0.99)
+    parser.add_argument('--n-step', type=int, default=3)
+    parser.add_argument('--target-update-freq', type=int, default=500)
     parser.add_argument('--epoch', type=int, default=50)
-    parser.add_argument('--step-per-epoch', type=int, default=1000)
+#    parser.add_argument('--epoch', type=int, default=5)
+    parser.add_argument('--step-per-epoch', type=int, default=100000)
+#    parser.add_argument('--step-per-epoch', type=int, default=100)
     # TODO why 8?
-    parser.add_argument('--step-per-collect', type=int, default=8)
-    # TODO understand where exactly this is used and why
-    # why is this a float?
-    parser.add_argument('--update-per-step', type=float, default=0.1)
-#    parser.add_argument('--update-per-step', type=float, default=0.6)
+    #parser.add_argument('--step-per-collect', type=int, default=12)
+    parser.add_argument('--step-per-collect', type=int, default=6)
+    # TODO having a different update frequency for the autoencoder 
+    # and the policy is probably a smart thing to do
+    #parser.add_argument('--update-per-step', type=float, default=0.1)
+    parser.add_argument('--update-per-step', type=float, default=1)
     parser.add_argument('--batch-size', type=int, default=64)
-    parser.add_argument('--training-num', type=int, default=8)
-#    parser.add_argument('--training-num', type=int, default=2)
-    # tests aren't necessary as we're free to overfit as much as we want
-    # the training domain IS the testing domain
-#    parser.add_argument('--test-num', type=int, default=8)
+    #parser.add_argument('--training-num', type=int, default=8)
+    parser.add_argument('--training-num', type=int, default=6)
+    #parser.add_argument('--test-num', type=int, default=8)
     parser.add_argument('--test-num', type=int, default=1)
     parser.add_argument('--logdir', type=str, default='log')
-    parser.add_argument('--log-name', type=str, default='rae_single-frame-trained_as_policy_1')
-#    parser.add_argument('--log-name', type=str, default='inverse_dynamics_model_1')
+    parser.add_argument('--log-name', type=str, default='dqn_rae_parallel_good_arch_fs_4_passing_q_grads')
     parser.add_argument('--render', type=float, default=0.)
     parser.add_argument(
         '--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu'
     )
     # NOTE: frame stacking needs to be 1 for what we're doing now
     # but let's keep it like a parameter here to avoid unnecessary code
-#    parser.add_argument('--frames-stack', type=int, default=2)
-#    parser.add_argument('--frames-stack', type=int, default=1)
+    # TODO you need to separate the frames-stack which the autoencoder gets
+    # from the stacking you give to the policy
+    # because the autoencoder as it is now does not account for velocity information
+    # which means that the policy has no velocity information and that's very important 
+    # when your goal is to learn how to manipulate dynamics
+    # TODO test reshaping the tensor in a shell until it works as expected
+    # i have no mental strength to do it now
+    #parser.add_argument('--frames-stack', type=int, default=2)
     parser.add_argument('--frames-stack', type=int, default=4)
     parser.add_argument('--resume-path', type=str, default=None)
     parser.add_argument('--resume-id', type=str, default=None)
@@ -95,11 +98,13 @@ def get_args():
 
 
 
-if __name__ == '__main__':
 #def test_dqn(args=get_args()):
+if __name__ == "__main__": 
     torch.set_num_threads(1)
     args=get_args()
     #env = make_atari_env(args)
+    # we have another way now, should be faster
+    #env, train_envs, test_envs = make_atari_env(
     train_envs, test_envs = make_atari_env(
         args.task,
         args.seed,
@@ -109,28 +114,26 @@ if __name__ == '__main__':
         frame_stack=args.frames_stack,
     )
     # this gives (1,84,84) w/ pixels in 0-1 range, as it should
+    #args.state_shape = env.observation_space.shape or env.observation_space.n
+    #args.action_shape = env.action_space.shape or env.action_space.n
     args.state_shape = train_envs.observation_space.shape or train_envs.observation_space.n
     args.action_shape = train_envs.action_space.shape or train_envs.action_space.n
     # should be N_FRAMES x H x W
     print("Observations shape:", args.state_shape)
     print("Actions shape:", args.action_shape)
     # make environments
-#    train_envs = ShmemVectorEnv(
-#        [lambda: make_atari_env(args) for _ in range(args.training_num)]
-#    )
-#    test_envs = ShmemVectorEnv(
-#        [lambda: make_atari_env_watch(args) for _ in range(args.test_num)]
-#    )
+    #train_envs = ShmemVectorEnv(
+    #    [lambda: make_atari_env(args) for _ in range(args.training_num)]
+    #)
+    #test_envs = ShmemVectorEnv(
+    #    [lambda: make_atari_env_watch(args) for _ in range(args.test_num)]
+    #)
     # seed
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
-    train_envs.seed(args.seed)
-    test_envs.seed(args.seed)
-    # in this experiment we're using the random policy
-    # which is just a placeholder really
-    rl_policy = RandomPolicy(args.action_shape)
-    observation_shape = args.state_shape
-
+    #train_envs.seed(args.seed)
+    #test_envs.seed(args.seed)
+    q_net = DQNNoEncoder(args.action_shape, args.frames_stack, args.device).to(args.device)
     if args.latent_space_type == 'single-frame-predictor':
         # in this case, we don't pass the stacked frames.
         # we unstack them, compress them, the stack the compressed ones and
@@ -138,18 +141,36 @@ if __name__ == '__main__':
         observation_shape = list(args.state_shape)
         observation_shape[0] = 1 
         observation_shape = tuple(observation_shape)
-    encoder = RAE_ENC(args.device, observation_shape, 
-            args.features_dim).to(args.device)
-    print(encoder)
-    decoder = RAE_DEC(args.device, observation_shape, 
-            args.features_dim).to(args.device)
-    print(decoder)
-#    encoder = CNNEncoderNew(observation_shape=args.state_shape, features_dim=args.features_dim, device=args.device).to(args.device)
-#    decoder = CNNDecoderNew(observation_shape=args.state_shape, n_flatten=encoder.n_flatten, features_dim=args.features_dim).to(args.device)
+        #encoder = CNNEncoderNew(observation_shape=observation_shape, 
+        #        features_dim=args.features_dim, device=args.device).to(args.device)
+        encoder = RAE_ENC(args.device, observation_shape, 
+                args.features_dim).to(args.device)
+        #decoder = CNNDecoderNew(observation_shape=observation_shape, 
+        #        n_flatten=encoder.n_flatten, features_dim=args.features_dim).to(args.device)
+        decoder = RAE_DEC(args.device, observation_shape, 
+                args.features_dim).to(args.device)
+    if args.pass_q_grads_to_encoder == False:
+        optim_q = torch.optim.Adam(q_net.parameters(), lr=args.lr)
+    else:
+        optim_q = torch.optim.Adam([{'params': q_net.parameters()}, 
+                {'params': encoder.parameters()}], lr=args.lr)
     optim_encoder = torch.optim.Adam(encoder.parameters(), lr=args.lr)
-    optim_decoder = torch.optim.Adam(decoder.parameters(), lr=args.lr)
-    #reconstruction_criterion = torch.nn.BCELoss()
-    reconstruction_criterion = torch.nn.MSELoss()
+    optim_decoder = torch.optim.Adam(decoder.parameters(), lr=args.lr, weight_decay=10**-7)
+    reconstruction_criterion = torch.nn.BCELoss()
+
+    rl_policy = DQNPolicy(
+        q_net,
+        optim_q,
+        args.gamma,
+        args.n_step,
+        target_update_freq=args.target_update_freq
+    )
+#    print(rl_policy.__class__)
+#    if isinstance(rl_policy, DQNPolicy):
+#        print("i4t is")
+#    print(type(rl_policy.__class__))
+#    exit()
+#    no_nex()
     # the rl_policy is then passed into our autoencoder-wrapper policy
     # it's done this way because the compression to latent spaces
     # comes before using the rl policy.
@@ -167,6 +188,7 @@ if __name__ == '__main__':
         args.pass_q_grads_to_encoder,
         args.alternating_training_frequency
     )
+    # TODO write this out
     if args.resume_path:
         policy.load_state_dict(torch.load(args.resume_path, map_location=args.device))
         print("Loaded agent from: ", args.resume_path)
@@ -195,6 +217,7 @@ if __name__ == '__main__':
     def save_fn(policy):
         torch.save(encoder.state_dict(), os.path.join(log_path, 'encoder.pth'))
         torch.save(decoder.state_dict(), os.path.join(log_path, 'decoder.pth'))
+        torch.save(q_net.state_dict(), os.path.join(log_path, 'q_net.pth'))
 
     def stop_fn(mean_rewards):
         if train_envs.spec.reward_threshold:
@@ -205,30 +228,30 @@ if __name__ == '__main__':
             return False
 
     # nature DQN setting, linear decay in the first 1M steps
-    # NOTE none of this is a thing here because we're using a random policy,
     def train_fn(epoch, env_step):
         pass
-        # and not q-learning
-#        if env_step <= 1e6:
-#            eps = args.eps_train - env_step / 1e6 * \
-#                (args.eps_train - args.eps_train_final)
-#        else:
-#            eps = args.eps_train_final
-#        policy.set_eps(eps)
-#        if env_step % 1000 == 0:
-#            logger.write("train/env_step", env_step, {"train/eps": eps})
+        if env_step <= 1e6:
+            eps = args.eps_train - env_step / 1e6 * \
+                (args.eps_train - args.eps_train_final)
+        else:
+            eps = args.eps_train_final
+        policy.set_eps(eps)
+        if env_step % 1000 == 0:
+            logger.write("train/env_step", env_step, {"train/eps": eps})
 
     def test_fn(epoch, env_step):
         pass
         # NOTE none of this is a thing here because we're using a random policy,
-        #policy.set_eps(args.eps_test)
+        policy.set_eps(args.eps_test)
 
     def save_checkpoint_fn(epoch, env_step, gradient_step):
         # see also: https://pytorch.org/tutorials/beginner/saving_loading_models.html
         ckpt_path_encoder = os.path.join(log_path, 'checkpoint_encoder_epoch_' + str(epoch) + '.pth')
         ckpt_path_decoder = os.path.join(log_path, 'checkpoint_decoder_epoch_' + str(epoch) + '.pth')
+        ckpt_path_q_net = os.path.join(log_path, 'checkpoint_q_net_epoch_' + str(epoch) + '.pth')
         torch.save({'encoder': encoder.state_dict()}, ckpt_path_encoder)
         torch.save({'decoder': decoder.state_dict()}, ckpt_path_decoder)
+        torch.save({'q_net': decoder.state_dict()}, ckpt_path_q_net)
         return "useless string for a useless return"
 
     # watch agent's performance
@@ -269,7 +292,7 @@ if __name__ == '__main__':
 
     # test train_collector and start filling replay buffer
     train_collector.collect(n_step=args.batch_size * args.training_num)
-    #train_collector.collect(n_step=args.buffer_size)
+    #train_collector.collect(n_step=args.buffer_size // 3)
 #    buffer.save_hdf5(os.path.join(log_path, 'buffer.h5'))
 #    print('buffer = saved to disk')
 #    exit()
