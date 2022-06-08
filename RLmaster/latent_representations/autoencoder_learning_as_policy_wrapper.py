626fdf06f58ccdd390a852f91dbdbd3d62ea3bae
m-guberina <gubi.guberina@gmail.com>
2022-06-08 20:19:20 +0200

    devices

--- RLmaster/latent_representations/autoencoder_learning_as_policy_wrapper.py
+++ RLmaster/latent_representations/autoencoder_learning_as_policy_wrapper.py
@@ -324,7 +324,10 @@ class AutoencoderLatentSpacePolicy(BasePolicy):
 
         #if self.latent_space_type == 'single-frame-predictor':
             #reconstruction_loss = self.reconstruction_criterion(decoded_obs, obs[:, -1, :, :].view(-1, 1, 84, 84))
-        reconstruction_loss = self.reconstruction_criterion(decoded_obs, obs / 255)
+        if self.latent_space_type == 'single-frame-predictor':
+            reconstruction_loss = self.reconstruction_criterion(decoded_obs, obs / 255)
+        if self.latent_space_type == 'single-frame-predictor':
+            reconstruction_loss = self.reconstruction_criterion(decoded_obs, obs_next / 255)
 #        if self.latent_space_type == 'forward-frame-predictor':
 #            batch.obs_next = torch.tensor(batch.obs_next, device=self.device)
 #            print(batch.obs_next.shape)
