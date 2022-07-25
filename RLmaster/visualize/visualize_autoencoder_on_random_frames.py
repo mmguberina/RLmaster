69c6e99d8c7f05fbef1e4fa1c6109283ae854feb
m-guberina <gubi.guberina@gmail.com>
2022-07-25 20:56:08 +0200

    i have no idea what this is

--- RLmaster/visualize/visualize_autoencoder_on_random_frames.py
+++ RLmaster/visualize/visualize_autoencoder_on_random_frames.py
@@ -21,8 +21,8 @@ from time import sleep
 # in the encoder class and stop this nonsense
 
 #features_dim = 576
-#features_dim = 3136
-features_dim = 50
+features_dim = 3136
+#features_dim = 50
 
 #log_path = "../../experiments/latent_only/log/PongNoFrameskip-v4/unlabelled_experiment/"
 #log_path = "../../experiments/latent_only/log/PongNoFrameskip-v4/training_preloaded_buffer_fs_1/"
@@ -30,7 +30,8 @@ features_dim = 50
 #log_path = "../../log/dqn_ae_parallel_good_arch_fs_4_passing_q_grads/"
 #log_path = "../../log/ae_single-frame-trained_as_policy_3136_4/"
 #log_path = "../../log/rae_single-frame-trained_as_policy_1/"
-log_path = "../../log/raibow_ae_parallel_good_arch_fs_4_passing_q_grads_6/"
+#log_path = "../../log/raibow_ae_parallel_good_arch_fs_4_passing_q_grads_6/"
+log_path = "../../log/latent_only/PongNoFrameskip-v4/ae_compressed-frame-trained_as_policy_3136/"
 #log_path = "../../experiments/latent_only/log/PongNoFrameskip-v4/ae_single-frame-trained_as_policy_3136/"
 #log_path_enc_dc = "../../experiments/latent_only/log/PongNoFrameskip-v4/"
 args = load_hyperparameters(log_path)
@@ -54,16 +55,16 @@ if args.latent_space_type == "forward-frame-predictor":
 #encoder.load_state_dict(torch.load("../../experiments/latent_only/encoder_features_dim_{}.pt".format(features_dim), map_location=torch.device('cpu')))
 #decoder.load_state_dict(torch.load("../../experiments/latent_only/decoder_features_dim_{}.pt".format(features_dim), map_location=torch.device('cpu')))
 
-encoder_name = "checkpoint_encoder_epoch_6.pth"
+encoder_name = "checkpoint_encoder_epoch_8.pth"
 decoder_name = "checkpoint_decoder_epoch_8.pth"
 #encoder_name = "encoder.pth"
 #decoder_name = "decoder.pth"
 #encoder_name = "encoder_features_dim_3136.pt"
 #decoder_name = "decoder_features_dim_3136.pt"
-#encoder = CNNEncoderNew(observation_shape=observation_shape, features_dim=features_dim, device='cpu')
-encoder = RAE_ENC("cpu", observation_shape, features_dim)
-#decoder = CNNDecoderNew(observation_shape=observation_shape, n_flatten=encoder.n_flatten, features_dim=features_dim)
-decoder = RAE_DEC("cpu", observation_shape, features_dim)
+encoder = CNNEncoderNew(observation_shape=observation_shape, features_dim=features_dim, device='cpu')
+#encoder = RAE_ENC("cpu", observation_shape, features_dim)
+decoder = CNNDecoderNew(observation_shape=observation_shape, n_flatten=encoder.n_flatten, features_dim=features_dim)
+#decoder = RAE_DEC("cpu", observation_shape, features_dim)
 #print(torch.load(log_path + encoder_name, map_location=torch.device('cpu')))
 encoder.load_state_dict(torch.load(log_path + encoder_name, map_location=torch.device('cpu'))['encoder'])
 decoder.load_state_dict(torch.load(log_path + decoder_name, map_location=torch.device('cpu'))['decoder'])
@@ -98,14 +99,15 @@ for i in range(5000):
     obs, reward, done, info = env.step(np.array([env.action_space.sample()]))
     #obs = torch.tensor(obs)
     #obs = torch.tensor(obs)[:,:-2,:,:].view(1, args.frames_stack, 84, 84)
-    obs = torch.tensor(obs)[:,-1,:,:].view(1,1, 84, 84)
+    #obs = torch.tensor(obs)[:,-1,:,:].view(1,1, 84, 84)
+    obs = torch.tensor(obs)
 #    print(obs.shape)
 #    obs = obs[:,-1,:,:].view(1,1,84,84) / 255
     #obs = np.ceil(obs * 255).astype(np.uint8).reshape((84,84))
     with torch.no_grad():
         #obs = decoder(encoder(obs.reshape((1,1,84,84))))
         obs = decoder(encoder(obs))
-    obs = obs.reshape((84,84)).numpy()
+    obs = obs[:,-1:,:].reshape((84,84)).numpy()
     if done:
         env.reset()
     cv2.imshow("cat", obs)
